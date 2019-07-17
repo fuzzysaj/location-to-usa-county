f5ead9d474ad8e7946589e19748303431833143a
Mark McCann <mamccan1@asu.edu>
2019-07-17 16:35:21 -0700

    alpha version, still not working

--- src/cmd-line-lookup.ts
+++ src/cmd-line-lookup.ts
@@ -3,12 +3,12 @@ import { locToCounty, County } from './';
 (async ()=> {
   if (process.argv.length <= 3) {
     console.log('Must pass in latitude as first command line argument and longitude as second.');
-    console.log('For example, to look up county for Phoenix: ./lookup 33.5038, -112.0253');
+    console.log('For example, to look up county for Phoenix: ./lookup 33.5038 -112.0253');
     return;
   }
   const latStr = process.argv[2];
   const lonStr = process.argv[3];
-  const floatRegEx = /^[-+]?[0-9]*\.?[0-9]+(?:[eE][-+]?[0-9]+)$/;
+  const floatRegEx = /^[-+]?[0-9]*\.?[0-9]+(?:[eE][-+]?[0-9]+)?$/;
   const errMsg = (x: string) => `Argument "${latStr}" does not look like a floating point number.`;
   if (!floatRegEx.test(latStr)) {
     console.log(errMsg(latStr));
